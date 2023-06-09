ae39dea23fdc9d42b62b031e331331776a9f7fd4
Rahul A B <rahulbushi69@gmail.com>
2023-06-09 20:55:17 +0530

    Updates to suite my needs

--- inject.js
+++ inject.js
@@ -6,7 +6,7 @@
         return
     }
 
-    const checkvalues = [ "41", "46", "51", "56", "61", "76", "81", "86", "91", "96", "101", "106","111", "116", "121", "126", "131", "136", "141", "146", "151", "156", "161","166", "171", "176", "181", "186", "191","151", "156", "161", "166", "171", "176", "181", "186", "191", "196"];
+    const checkvalues = ["6", "9", "12", "16", "19", "23", "27", "29", "33", "37", "41", "51", "56", "61", "76", "81", "86", "91", "96", "101", "106", "111", "121", "126", "131", "136", "141", "146", "151", "156", "161", "166", "171", "181", "186", "191", "196"];
     checkvalues.forEach(value => {
         const element = document.querySelector('input[value="${value}"]');
         if (element) {
@@ -14,9 +14,6 @@
         }
     });
 
-    const submitButton = document.querySelector('input[value="Submit"]');
-    if (submitButton) {
-        submitButton.disabled = false;
-    }
+    document.querySelector('input[value="Submit"]').disabled = false;
 
 })();
